45ea97ce2b99f2d94290ca232f53ba4c8d25412b
Christopher Chamaletsos <webmaster@kantoliana.gr>
2025-05-08 14:44:27 +0300

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# daggerverse
+# Dagger CLI
+dagger call get-container-from-repo \
+  --pat=$PAT \
+  --gitUserEmail=$gitUserEmail \
+  --gitUserName="$gitUserName" \
+  --environment=$environment \
+  --project=$project \
+  --repo=$repo \
+  --branch=$branch \
+  --namespace=$namespace \
+  --aksFolderToCreate=$aksFolderToCreate \
+  --parentApp=$parentApp
+
+
+# Dagger Shell
+get-container-from-repo $PAT $gitUserEmail $gitUserName $environment $project $repo $branch $namespace $aksFolderToCreate $parentApp | terminal 
--- main.go
+++ main.go
@@ -1,38 +1,106 @@
-// A generated module for Daggerverse functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
 	"context"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/chrishham/daggerverse/internal/dagger"
 )
 
 type Daggerverse struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *Daggerverse) ContainerEcho(ctx context.Context, stringArg string) (string, error) {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg}).Stdout(ctx)
+// Returns a container with the input repo cloned
+func (m *Daggerverse) GetContainerFromRepo(
+	ctx context.Context,
+	pat,
+	gitUserEmail,
+	gitUserName,
+	environment,
+	project,
+	repo,
+	branch,
+	namespace,
+	aksFolderToCreate,
+	parentApp string,
+) *dagger.Container {
+	repoUrl := fmt.Sprintf("https://%s@dev.azure.com/chamaletsoschrist/%s/_git/%s", pat, project, repo)
+	aksYamlRepoUrl := fmt.Sprintf("https://%s@dev.azure.com/chamaletsoschrist/DevOps_Private/_git/AKS", pat)
+
+	repoContainer := dag.Container().
+		From("alpine:latest").
+		WithExec([]string{"apk", "add", "git"}).
+		WithExec([]string{"git", "config", "--global", "user.email", gitUserEmail}).
+		WithExec([]string{"git", "config", "--global", "user.name", gitUserName}).
+		WithEnvVariable("CACHEBUSTER", time.Now().String()).
+		WithExec([]string{"git", "clone", aksYamlRepoUrl}).
+		WithExec([]string{"git", "clone", repoUrl}).
+		WithWorkdir(repo)
+
+	defaultBranch, _ := repoContainer.
+		WithExec([]string{
+			"sh", "-c",
+			"git remote show origin | grep 'HEAD branch' | cut -d' ' -f5",
+		}).Stdout(ctx)
+	fmt.Println("defaultBranch :", defaultBranch)
+	// Read the original file content
+	fileContent, _ := repoContainer.WithExec([]string{"cat", "/AKS/01_parent_file.yaml"}).Stdout(ctx)
+
+	// Replace all variables directly using the function parameters
+
+	variables := map[string]string{
+		"parentApp":   parentApp,
+		"environment": environment,
+		"project":     project,
+		"repo":        repo,
+		"branch":      branch,
+		"namespace":   namespace,
+		"repoUrl":     repoUrl,
+		// Add any other variables you need to replace
+	}
+
+	for key, value := range variables {
+		fileContent = strings.ReplaceAll(fileContent, "$("+key+")", value)
+	}
+
+	parentFile := fmt.Sprintf("/%s/%s/%s-%s.yaml", repo, aksFolderToCreate, parentApp, environment)
+	repoContainer = repoContainer.
+		WithExec([]string{"mkdir", "-p", aksFolderToCreate}).
+		WithNewFile("/tmp/modified_file.yaml", fileContent).
+		WithExec([]string{"cp", "/tmp/modified_file.yaml", parentFile})
+		// WithExec([]string{"cp", "/AKS/01_parent_file.yaml", parentFile})
+
+	repoContainer = CommitAndPush(repoContainer, branch, defaultBranch, `Adding application file for `+parentApp)
+
+	return repoContainer
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *Daggerverse) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
+func CommitAndPush(container *dagger.Container, branch, defaultBranch, commitMsg string) *dagger.Container {
+	return container.WithExec([]string{"git", "checkout", branch}).
+		WithExec([]string{"git", "pull", "origin", branch}).
+		WithExec([]string{"git", "add", "-A", "."}).
+		WithExec([]string{"sh", "-c", `
+if [[ -n "$(git status -s)" ]]; then
+  git commit -m "` + commitMsg + `"
+  git push origin ` + defaultBranch + `
+else
+  echo "No changes to commit"
+fi`})
+}
+
+// substituteVars replaces all $(varName) in input with corresponding values from vars map.
+func substituteVars(input string, vars map[string]string) string {
+	// Regex to match $(varName)
+	re := regexp.MustCompile(`\$\(([^)]+)\)`)
+
+	// Replace all matches with corresponding values from map
+	return re.ReplaceAllStringFunc(input, func(match string) string {
+		key := re.FindStringSubmatch(match)[1]
+		if val, ok := vars[key]; ok {
+			return val
+		}
+		return match // leave untouched if not found
+	})
 }
